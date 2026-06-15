9d78114a1dce02bced85fbc9d5acfe66e0ec0d22
Filip Sapina <filip.sapina@hobsonvillepoint.school.nz>
2026-06-16 10:12:08 +1200

    created system for second variation to work and created example section.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
+let SECTIONS = null;
 function getShapePoints(sides, randomazzo) {
   const points = [];
 
-  const width = 80;
-  const height = 50;
+  const width = 100;
+  const height = 100;
 
   for (let i = 0; i < sides; i++) {
       const t = i / sides;
@@ -55,23 +56,57 @@ function PointsToClippath(points) {
 
     return result;
 }
-// document.addEventListener("DOMContentLoaded", () => {
-//   for (let i = 0; i < 101; i += 2) {
-//     let element = document.createElement("div");
-//     element.style.backgroundImage = "linear-gradient(white, black)";
-//     element.style.display = "flex";
-//     element.style.justifyContent = "center";
-//     element.style.textAlign = "center"
-//     element.style.padding = "100px";
-//     element.style.alignItems = "center";
-//     element.style.color = "white";
-//     element.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet, consectetu" 
-//     ;
-//     element.style.height = "500px";
-//     element.style.width = "500px";
+ document.addEventListener("DOMContentLoaded", () => {
+    SECTIONS = document.getElementById("sections");
 
-//     element.style.clipPath = `polygon(${PointsToClippath(getShapePoints(100, i))})`;
+    new Section("banana",
+        "This waffling session is shorter than the upcoming section both of which have no real relevance to the game itself. If you've continued reading past the first sentance I'll let you in on a little secret, I wrote this section after writing the following section and as such I can make refrences to without you understanding it. the game revolves around a new technology that lets people work and browse the internet in their sleep which at the time this was set in early internet this *might* have seemed cool by today I think most people would have seen it as dystopian",
+        "This is incredible and very ununtemporary text that explains in much detail how the game is incredible and you have to buy it right now. lots of rainbows and magic were put into this game so that it truly is the perfect euphoric experince to play through it. People that don't play this game might not be real so you need to watch out for people that don't play this game. Instead you should trust people that waffle to fill a text box -> I think that a very big issue in current website design is the overeliance on the minimilist aesthetic to make the design look clean and perfect which the most people generally like so companies tend to choose it for their websites but I think smaller companies and small groups should try experimenting, I played this game hypnospace outlaw that tries to emulate the old internet aesthetic which I was obviosuly not there for and it looks like it was a lot of fun, another hinderence to this though is that most apps and places to meet peoople are now specific websites which is a somewhat modern concept since it used to be that you could make your own website to host so that people can figure out who you are easier and I think sites like twitter and facebook take a lot of that intersting aspect of the internet away.",
+        "https://cdn.britannica.com/88/256588-050-069B5B0F/Dante-Alighieri-portrait-Divine-Comedy-Inferno.jpg?w=400&h=300&c=crop",
+        "red", "radial-gradient(green, purple)", "orange", 6, "magenta"   
+    );
 
-//     document.getElementById("sections").appendChild(element);
-//   }
-// })
+    
+
+})
+
+
+class Section {
+    constructor(title, text_1, text_2, img_src, title_colour, paper_bg, text_colour, random_factor, bg_colour) {
+        let e_section = document.createElement("div");
+        e_section.style.backgroundColor = bg_colour;
+        e_section.classList.add("dynamic-section");
+
+        let e_title = document.createElement("h1");
+        e_title.textContent = title;
+        e_title.style.color = title_colour;
+        e_title.classList.add("section-title");
+        e_section.appendChild(e_title);
+
+        let e_paper_1 = document.createElement("div")
+        e_paper_1.textContent = text_1;
+        e_paper_1.style.backgroundImage = paper_bg;
+        e_paper_1.style.color = text_colour;
+        e_paper_1.classList.add("torn-paper", "paper-1");
+        e_paper_1.style.clipPath = `polygon(${PointsToClippath(getShapePoints(100, random_factor))})`;
+        e_section.appendChild(e_paper_1);
+        
+        let e_paper_2 = document.createElement("div")
+        e_paper_2.textContent = text_2;
+        e_paper_2.style.backgroundImage = paper_bg;
+        e_paper_2.style.color = text_colour
+        e_paper_2.classList.add("torn-paper", "paper-2");
+        e_paper_2.style.clipPath = `polygon(${PointsToClippath(getShapePoints(100, random_factor))})`;
+        e_section.appendChild(e_paper_2);
+
+        let e_img = document.createElement("img");
+        e_img.src = img_src;
+        e_img.classList.add("section-image");
+        e_section.appendChild(e_img);
+
+
+        SECTIONS.appendChild(e_section);
+
+
+    }
+}
